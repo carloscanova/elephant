a24547fa8508f38c4f25fdd90e4dfe01f5fdb180
pietroquaglio <p.quaglio@fz-juelich.de>
2017-03-29 16:21:19 +0200

    Checked if input is spiketrains, times converted to quantity array before using numpy.diff

--- elephant/statistics.py
+++ elephant/statistics.py
@@ -46,7 +46,10 @@ def isi(spiketrain, axis=-1):
     """
     if axis is None:
         axis = -1
-    intervals = np.diff(spiketrain, axis=axis)
+    if isinstance(spiketrain, np.ndarray):
+        intervals = np.diff(spiketrain, axis=axis)
+    else:
+        intervals = np.diff(spiketrain.times.view(pq.Quantity), axis=axis)
     if hasattr(spiketrain, 'waveforms'):
         intervals = pq.Quantity(intervals.magnitude, units=spiketrain.units)
     return intervals
@@ -608,13 +611,13 @@ def instantaneous_rate(spiketrain, sampling_period, kernel='auto',
         The kernel is used for convolution with the spike train and its
         standard deviation determines the time resolution of the instantaneous
         rate estimation.
-        Default: 'auto'. In this case, the optimized kernel width for the 
+        Default: 'auto'. In this case, the optimized kernel width for the
         rate estimation is calculated according to [1] and with this width
-        a gaussian kernel is constructed. Automatized calculation of the 
+        a gaussian kernel is constructed. Automatized calculation of the
         kernel width is not available for other than gaussian kernel shapes.
     cutoff : float
         This factor determines the cutoff of the probability distribution of
-        the kernel, i.e., the considered width of the kernel in terms of 
+        the kernel, i.e., the considered width of the kernel in terms of
         multiples of the standard deviation sigma.
         Default: 5.0
     t_start : Time Quantity (optional)
